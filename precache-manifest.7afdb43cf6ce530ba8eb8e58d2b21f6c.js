88cda7cd8cb45cb81886ce79dbe4c64ddedfd0b2
Deepali Singh <14singh.deepali@gmail.com>
2019-08-08 13:05:08 -0500

    Updates

---
+++ precache-manifest.7afdb43cf6ce530ba8eb8e58d2b21f6c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517035ab9c3c36a12beeae432e55db84",
+    "url": "/Clicky-Barbie-Game/index.html"
+  },
+  {
+    "revision": "f769a9d3504fb1a07cd9",
+    "url": "/Clicky-Barbie-Game/static/css/main.3a2390e2.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Clicky-Barbie-Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "f769a9d3504fb1a07cd9",
+    "url": "/Clicky-Barbie-Game/static/js/main.f85dab75.chunk.js"
+  },
+  {
+    "revision": "5358e413684a056625c7",
+    "url": "/Clicky-Barbie-Game/static/js/runtime~main.d7453c80.js"
+  }
+]);
